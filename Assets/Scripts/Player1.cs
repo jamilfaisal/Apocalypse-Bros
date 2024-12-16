4ad2ecd2778c5b22d4298a7386f72994a56f2956
CooperativeDev <Farshad.Q@hotmail.com>
2024-12-15 21:56:31 -0500

    initial upload

---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    private PlayerAttributes attributes;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        attributes = new PlayerAttributes();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerAttributes.cs
@@ -0,0 +1,24 @@
+namespace DefaultNamespace;
+
+public class PlayerAttributes
+{
+    private string name;
+    private int level;
+    private int exp;
+    private int maxHealth;
+    private int currentHealth;
+    private int healthRegenerationRate;
+    private int damage;
+    private int reloadRate;
+
+    public PlayerAttributes()
+    {
+        level = 1;
+        exp = 0;
+        maxHealth = 100;
+        currentHealth = 100;
+        healthRegenerationRate = 0;
+        damage = 1;
+        reloadRate = .5;
+    }
+}
