b8c2bfe6fb9627ad13cb701f5a3708c8940df0d6
Faisal Jamil <jamilfaisal43@gmail.com>
2024-12-20 04:57:20 -0500

    Remove unused line

--- Assets/Scripts/Mob/MobAbstract.cs
+++ Assets/Scripts/Mob/MobAbstract.cs
@@ -1,11 +1,9 @@
-using Managers;
 using UnityEngine;
 
 namespace Mob
 {
     public abstract class MobAbstract : MonoBehaviour
     {
-        protected PlayerLevelManager LevelManager;
         protected int Health;
 
         public void TakeDamage(int damage)
