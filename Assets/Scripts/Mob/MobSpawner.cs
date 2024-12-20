959f09fc547b66876b5bcb32be5d59dc57993bc1
CooperativeDev <Farshad.Q@hotmail.com>
2024-12-20 03:37:10 -0500

    addressed review comments cool i am just a salesman. ok thanks

--- Assets/Scripts/Mob/MobSpawner.cs
+++ Assets/Scripts/Mob/MobSpawner.cs
@@ -7,19 +7,14 @@ namespace Mob
     public class MobSpawner : MonoBehaviour
     {
         [SerializeField] private GameObject mobPrefab;
-        private Vector3 _floorDimensions;
         private int _radius;
-        private double _randomAngleRadians;
-        private int _xCoordinate;
-        private int _zCoordinate;
         private double _timeSinceLastSpawn;
         private readonly Random _randomNumber = new Random();
         private const int DistanceFromMapEdge = 10;
 
         private void Start()
         {
-            _floorDimensions = transform.localScale;
-            _radius = ((int)_floorDimensions.x / 2) - DistanceFromMapEdge;
+            _radius = ((int)transform.localScale.x / 2) - DistanceFromMapEdge;
         }
 
         private void Update()
@@ -32,15 +27,17 @@ namespace Mob
 
         private void SpawnMob()
         {
-            MobSpawnCoordinates();
-            Instantiate(mobPrefab, new Vector3(_xCoordinate, 10, _zCoordinate), Quaternion.identity);
+            Vector3 mobSpawnCoordinates = CalculateMobSpawnCoordinates();
+            Instantiate(mobPrefab, mobSpawnCoordinates, Quaternion.identity);
         }
 
-        private void MobSpawnCoordinates()
+        private Vector3 CalculateMobSpawnCoordinates()
         {
-            _randomAngleRadians = _randomNumber.Next(360)*(Math.PI/180);
-            _xCoordinate = (int)(_radius * Math.Sin(_randomAngleRadians));
-            _zCoordinate = (int)(_radius * Math.Cos(_randomAngleRadians));
+            double randomAngleRadians = _randomNumber.Next(360)*(Math.PI/180);
+            int xCoordinate = (int)(_radius * Math.Sin(randomAngleRadians));
+            int zCoordinate = (int)(_radius * Math.Cos(randomAngleRadians));
+            int yCoordinate = (int)(transform.position.y + 10);
+            return new Vector3(xCoordinate, yCoordinate, zCoordinate);
         }
 
         private bool SpawnTimer()
